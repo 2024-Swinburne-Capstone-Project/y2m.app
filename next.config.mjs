ca5019f52d468dc7d510be274d253cc4606e4314
Alexander Kyriacou <92493791+AlexKyriacou@users.noreply.github.com>
2024-04-10 21:53:31 +1000

    feat: :wrench: add logging on fetches (#6)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
-    output: 'standalone'
+  // https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
+  output: "standalone",
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
