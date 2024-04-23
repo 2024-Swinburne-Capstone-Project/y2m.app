7732279529268a6d178ec074ea8db44fd4d76228
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-04-23 21:26:37 +1000

    feat: add carousel to about page

---
+++ src/config/about.ts
@@ -0,0 +1,50 @@
+import { AboutConfig } from "@/types";
+
+export const aboutConfig: AboutConfig = {
+  heroSection: {
+    title: "About Us",
+    imagePath: "/about/about-us.png",
+  },
+  heroContent: {
+    titleText:
+      "At You2Mentor, we believe that everyone should have the opportunity to access a mentor",
+    contentText:
+      "With the goal of making mentorship accessible to all, You2Mentor was launched in December 2022 to be a dedicated online resource for personal growth. It provides users with the necessary tools and guidance to help them reach their potential.",
+    imagePath: "/about/about-us.png",
+  },
+  additionalContent: {
+    contentBody:
+      "According to Gallup, only 37% of individuals currently have access to at least one mentor. So we have created a platform for individual development where not only can you drive your growth, you can utilise a tribe of mentors based on development goals and mentor others utilising your strengths Signup to take the next step in your development journey",
+  },
+  carouselSlides: [
+    {
+      title: "Keep It Simple",
+      content:
+        "Our Aim Is To Make It Simple To Drive Individual Development For Individuals And Organisations.",
+      imagePath: "/about/about-us.png",
+    },
+    {
+      title: "Community For Individual Growth",
+      content:
+        "Facilitate A Community That Supports And Celebrates Each Others Achievements.",
+      imagePath: "/about/about-us.png",
+    },
+    {
+      title: "Empower Others",
+      content: "In Order To Achieve Greatness, We Need To Dream Big",
+      imagePath: "/about/about-us.png",
+    },
+    {
+      title: "Be A Force For Good",
+      content:
+        "Having A Mentor Is Considered A Luxury In Low And Middle Income Countries (LMIC). We Aim To Bridge This Gap To Open Up Opportunities To Grow",
+      imagePath: "/about/about-us.png",
+    },
+    {
+      title: "Learning And Innovation",
+      content:
+        "We Believe That Life Is A Continuous Journey Of Learning Which Leads To Innovation And Thinking Outside The Norm",
+      imagePath: "/about/about-us.png",
+    },
+  ],
+};
