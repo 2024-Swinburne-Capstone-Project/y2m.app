43299a8f25115cb939028b93c17e51c490b6595e
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-04-23 17:59:40 +1000

    feat: add hero content component

---
+++ src/app/about/components/hero-content-image.tsx
@@ -0,0 +1,28 @@
+// hero-content-image.tsx
+import React from "react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+interface HeroContentImageProps {
+  imagePath: string;
+  className?: string;
+}
+
+const HeroContentImage: React.FC<HeroContentImageProps> = ({
+  imagePath,
+  className,
+}) => {
+  return (
+    <div className={cn("md:w-1/2", className)}>
+      <Image
+        src={imagePath}
+        width={1280}
+        height={300}
+        alt="Hero Section"
+        className="w-full h-auto object-cover"
+      />
+    </div>
+  );
+};
+
+export default HeroContentImage;
