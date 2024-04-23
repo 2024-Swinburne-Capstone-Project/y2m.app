0e74a76846de5ad3a95b1d3f8b114a94cb8c7823
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-04-23 17:11:05 +1000

    feat: add hero section and about me example

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/components/hero-section";
+
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <HeroSection title="About Us" imagePath="/about/about-us.jpg" />
+    </div>
+  );
+}
