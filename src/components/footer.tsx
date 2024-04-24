eb1561ae6a70f45c18d37f16279c21cf1e1682d6
Marella Morad <marella99mourad@gmail.com>
2024-04-25 02:28:51 +1000

    feat: add socials to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,22 +1,77 @@
 import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faTiktok,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
   return (
     <footer className="border-t py-8">
       <div className="container mx-auto flex flex-col items-center justify-between px-4 sm:flex-row">
+        <Image
+          src={"/y2m-logo.png"}
+          alt="You2Mentor"
+          width={80}
+          height={80}
+          className="mr-5"
+        />
         <p className="text-center text-sm text-muted-foreground sm:text-left">
           &copy; {new Date().getFullYear()} You2Mentor. All rights reserved.
         </p>
+        <div className="flex gap-6">
+          <Link href="https://www.linkedin.com/company/you2mentor/">
+            <FontAwesomeIcon
+              className="w-5"
+              icon={faLinkedin}
+              style={{ color: "hsl(var(--muted-foreground))" }}
+            />
+          </Link>
+          <Link href="https://www.twitter.com/You2Mentor">
+            <FontAwesomeIcon
+              className="w-5"
+              icon={faXTwitter}
+              style={{ color: "hsl(var(--muted-foreground))" }}
+            />
+          </Link>
+          <Link href="https://www.instagram.com/You2Mentor/">
+            <FontAwesomeIcon
+              className="w-5"
+              icon={faInstagram}
+              style={{ color: "hsl(var(--muted-foreground))" }}
+            />
+          </Link>
+          <Link href="https://www.tiktok.com/@you2mentor?_t=8lnc5akDhHG&_r=1">
+            <FontAwesomeIcon
+              className="w-5"
+              icon={faTiktok}
+              style={{ color: "hsl(var(--muted-foreground))" }}
+            />
+          </Link>
+          <Link href="https://www.facebook.com/You2mentor/">
+            <FontAwesomeIcon
+              className="w-5"
+              icon={faFacebook}
+              style={{ color: "hsl(var(--muted-foreground))" }}
+            />
+          </Link>
+        </div>
         <nav className="mt-4 flex space-x-4 sm:mt-0">
           <Link
-            href="#"
-            className="text-sm text-muted-foreground hover:underline">
-            Terms of Service
+            href="/legal"
+            className="text-sm text-muted-foreground hover:underline"
+          >
+            Privacy Policy
           </Link>
           <Link
-            href="#"
-            className="text-sm text-muted-foreground hover:underline">
-            Privacy Policy
+            href="/legal"
+            className="text-sm text-muted-foreground hover:underline"
+          >
+            Terms of Service
           </Link>
         </nav>
       </div>
