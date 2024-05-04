4b22e676823b61cede72b3446e7605fd6a86a079
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-05-04 12:17:21 +1000

    feat: config auth0 to route to home on login

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,11 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    // https://auth0.github.io/nextjs-auth0/modules/handlers_login.html
+    returnTo: '/home',
+  }),
+});
 
 //  This creates the following routes:
 //  /api/auth/login: The route used to perform login with Auth0.
