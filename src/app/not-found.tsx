be9b15f65ce431e96b1c2792b87985ada08886d1
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-05-09 17:21:39 +1000

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { notFoundConfig } from '@/config/not-found';
+
+const NotFoundPage = () => {
+  return (
+    <div className="mx-6 mb-6 min-h-screen bg-background">
+      <div className="mx-auto max-w-7xl space-y-4 text-center">
+        <Image
+          src={notFoundConfig.imageSource}
+          alt={notFoundConfig.imageAlt}
+          width={300}
+          height={300}
+          className="mx-auto mb-8 mt-20"
+        />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          {notFoundConfig.title}
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">{notFoundConfig.description}</p>
+        <Button asChild>
+          <Link href={notFoundConfig.buttonHref}>{notFoundConfig.buttonText}</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
