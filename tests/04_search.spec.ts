cf7c0df11c5386eb84d99569bce0b11a4afc4e14
Lachlan Martin <lachlan.martin64@gmail.com>
2024-10-24 02:32:59 +1100

    testing fix

--- tests/04_search.spec.ts
+++ tests/04_search.spec.ts
@@ -9,8 +9,10 @@ test('Search by skill', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await page.waitForTimeout(1000);
   await page.getByPlaceholder('Search...').fill('React');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
 
@@ -22,8 +24,10 @@ test('Search by name', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await page.waitForTimeout(1000);
   await page.getByPlaceholder('Search...').fill('Test Mentor');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
 
@@ -35,8 +39,10 @@ test('Search by education - 01', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
-  await page.getByPlaceholder('Search...').fill('Swinburne');
+  await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Search...').fill('Monash');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
 
@@ -48,8 +54,10 @@ test('Search by education - 02', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
-  await page.getByPlaceholder('Search...').fill('Bachelor of Engineering');
+  await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Search...').fill('Computer Science');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
 
@@ -61,8 +69,10 @@ test('Search by education - 03', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
-  await page.getByPlaceholder('Search...').fill('Software');
+  await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Search...').fill('Software Development');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
 
@@ -74,8 +84,10 @@ test('Search by experience - 01', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
-  await page.getByPlaceholder('Search...').fill('NAB');
+  await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Search...').fill('Atlassian');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
 
@@ -87,7 +99,9 @@ test('Search by experience - 02', async ({ page }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('bnv8ZMQ@kbd8tah2gta');
   await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await page.waitForTimeout(1000);
   await page.getByPlaceholder('Search...').fill('Software Engineer');
   await page.getByPlaceholder('Search...').press('Enter');
+  await page.waitForTimeout(1000);
   await expect(page.getByText('Test Mentor')).toBeVisible();
 });
